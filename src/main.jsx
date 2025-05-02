0f456605277018a7f62d5f24e63e6db6f5d054fb
tracylick61 <dgrudanov123@gmail.com>
2025-05-02 18:31:39 +0300

    first commit

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,8 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-
+import 'materialize-css/dist/css/materialize.min.css';
+import 'materialize-css/dist/js/materialize.min.js';
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
